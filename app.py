a5df50bb714887ca7c62768c045db9f2bf7c5e1e
Jademir de Moura <jademirf@gmail.com>
2021-11-16 20:01:58 -0300

    add recurso pessoas/:id ao app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from models import Pessoas
+from flask.json import jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,7 +18,14 @@ class Pessoa(Resource):
         except:
             return { 'erro': 'Nenhum usuario identificado com o id informado.'}
 
+class Usuarios(Resource):
+    def get(self):
+        pessoas = Pessoas.query.all()
+        return pessoas
+
+
 api.add_resource(Pessoa, '/pessoas/<int:id>')
+# api.add_resource(Usuarios, '/pessoas')
 
 if __name__ == '__main__':
     app.run(debug=True)
