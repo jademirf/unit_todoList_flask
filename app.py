19a9498b0ef6d2d8b3de3bd0bdf9a8cb4c835906
Jademir de Moura <jademirf@gmail.com>
2021-11-30 20:33:02 -0300

    add jwt encode and decode

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import json
+from werkzeug.security import generate_password_hash
+
+import jwt
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, request
 from models import Pessoas
 from flask.json import jsonify
 
@@ -26,9 +30,36 @@ class Usuarios(Resource):
         pessoa = Pessoas(nome='Desmennyellysson', idade=30)
         pessoa.save()
 
+class Auth(Resource):
+    def post(self):
+        try:
+            key = "senhaSuperSecretaESegura"
+            user = json.loads(request.data)
+            pass_hash = generate_password_hash(user["pass"])
+            payload = {
+                "user": "Desmennyellysson Jerry",
+                "login": user['login'],
+                "level": 5
+            }
+            jwtToken = jwt.encode(payload, key, algorithm="HS256")
+            response = {
+                "jwt-token": jwtToken
+            }
+            return response
+        except:
+            return {"error": "Bad request"}
+
+    def get(self):
+        try:
+            token = request.headers.get('authorization')
+            payload = jwt.decode(token, "senhaSuperSecretaESegura", algorithms=["HS256"])
+            return {"status": "ok", "payload": payload}
+        except:
+            return {"error": "deu ruim"}
 
 api.add_resource(Pessoa, '/pessoas/<int:id>')
 api.add_resource(Usuarios, '/pessoas')
+api.add_resource(Auth, '/signin')
 
 if __name__ == '__main__':
     app.run(debug=True)
